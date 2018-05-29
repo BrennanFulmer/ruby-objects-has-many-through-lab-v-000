35a12d35e92f36a87dc7ed230e8890d6fc272a14
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-29 00:47:17 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,7 +12,7 @@ class Doctor
     @@all
   end
 
-  def new_appointment(date, patient)
+  def new_appointment(patient, date)
     item = Appointment.new(patient, self, date)
     item.doctor = self
     item
